efc4ce983502cef4e7a5d41c9a44a7c5929bbe70
svcastaneda <svcastaneda@gmail.com>
2015-11-14 00:22:08 -0600

    Add solution to concatenate arrays challenge

---
+++ week-4/concatenate-arrays/my_solution.rb
@@ -0,0 +1,21 @@
+# Concatenate Two Arrays
+
+# I worked on this challenge [by myself, with: ].
+
+
+# Your Solution Below
+
+def array_concat(array_1, array_2)
+  # Your code here
+  newArray = []
+  
+  array_1.each do |item|
+    newArray.push(item)
+  end
+  
+  array_2.each do |item|
+    newArray.push(item)
+  end
+  
+  return newArray
+end
