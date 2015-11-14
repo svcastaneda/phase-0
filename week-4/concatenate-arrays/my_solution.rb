ddcec6607cd5ede0f36de980ac5c5036ef86e55d
svcastaneda <svcastaneda@gmail.com>
2015-11-14 00:25:06 -0600

    Shorter solution to challenge

--- week-4/concatenate-arrays/my_solution.rb
+++ week-4/concatenate-arrays/my_solution.rb
@@ -7,15 +7,16 @@
 
 def array_concat(array_1, array_2)
   # Your code here
-  newArray = []
+  # newArray = []
   
-  array_1.each do |item|
-    newArray.push(item)
-  end
-  
-  array_2.each do |item|
-    newArray.push(item)
-  end
+  # array_1.each do |item|
+#     newArray.push(item)
+#   end
+#
+#   array_2.each do |item|
+#     newArray.push(item)
+#   end
+  newArray = array_1.concat(array_2)
   
   return newArray
 end
